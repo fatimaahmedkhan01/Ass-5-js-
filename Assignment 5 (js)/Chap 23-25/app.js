3c2ae96b59eba697908c3d402d181ef85f3a65b7
fatimaahmedkhan01 <fatimaahmed13k@gmail.com>
2025-09-23 17:37:44 +0500

    Add files via upload

---
+++ Assignment 5 (js)/Chap 23-25/app.js
@@ -0,0 +1,30 @@
+// 1st question
+// var string = "Pakistani";
+// alert("String:"+ " " +string);
+// alert("Index of 'n':"+string.indexOf("n"));
+// 1st question
+
+// 2nd question
+// var word = "Hello World";
+// alert("String : " + word);
+// alert("Last index of 'l' : " + word.lastIndexOf("l"));
+// 2nd question
+
+// 3rd question
+// var word = "Pakistani";
+// alert("String : "+word);
+// alert("Character at 3rd index : "+word[3]);
+// 3rd question
+
+// 4th question
+// var firstName = prompt("Enter your first name: ");
+// var lastName = prompt("Enter your last name: ");
+// alert("Hello!"+" "+firstName+" "+lastName);
+// 4th question
+
+// 5th question
+// var city = "Hyderabad";
+// var remaining = city.slice(5,9);
+// var city2 = "Islam";
+// alert(city2+remaining);
+// 5th question
