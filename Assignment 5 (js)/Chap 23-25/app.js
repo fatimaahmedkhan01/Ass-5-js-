6601c904d22bc7fd7d8b61cdd8d4a63329b539ca
fatimaahmedkhan01 <fatimaahmed13k@gmail.com>
2025-09-26 12:37:22 +0500

    Add files via upload

--- Assignment 5 (js)/Chap 23-25/app.js
+++ Assignment 5 (js)/Chap 23-25/app.js
@@ -28,3 +28,138 @@
 // var city2 = "Islam";
 // alert(city2+remaining);
 // 5th question
+
+// 6th question
+// var massage = "Ali and sami are best friends.They play cricket and football together.";
+// alert(massage.replaceAll("and","&"));
+// 6th question
+
+// 7th question
+// var num = "472";
+// document.write("Value : " + num + "<br>" + "Type : " + typeof(num) + "<br>" );
+// var num = 472;
+// document.write("Value : " + num + "<br>" + "Type : " + typeof(num));
+// 7th question
+
+// 8th question
+// var input = prompt("Enter something in lower case");
+// var uppercase = input.toUpperCase();
+// alert(uppercase);
+// 8th question
+
+// 9th question
+// var input = prompt("Enter anything");
+// var titleCase = (input[0].toUpperCase());
+// var length = input.length-1;
+// document.write(titleCase);
+// for(var i=1; i<=length; i++){
+//     document.write(input[i]);
+// }
+// 9th question
+
+// 10th question
+// var num = 35.36;
+// alert("Number : " + num);
+// var str = (num.toString());
+// alert("Result : " + str);
+// 10th question
+
+// 11th question
+// var userName = prompt("Enter Username");
+// var length = userName.length;
+// for(var i=0; i<=length; i++){
+//     if(userName[i]==="!" || userName[i]==="," || userName[i]==="." || userName[i]==="@"){
+//         alert("Please enter a valid username");
+//     }
+// }
+// 11th question
+
+// 12th question
+// var arr = ["cake","apple pie","cookie","chips","patties"];
+// var index;
+// var input = prompt("Welcome to ABC Bakery.What do you want to order sir/ma'am?").toLowerCase();
+// if(input==="cake"){
+//     index=0;
+// }
+// else if(input==="apple pie"){
+//     index=1;
+// }
+// else if(input==="cookie"){
+//     index=2;
+// }
+// else if(input==="chips"){
+//     index=3;
+// }
+// else if(input==="patties"){
+//     index=4;
+// }
+
+
+// if(input==="cake" || input==="apple pie" || input==="cookie" || input==="chips" || input==="patties"){
+//     alert(input+" is availaible at index "+index+" at our Bakery");
+// }
+// else{
+//     alert("We are sorry "+input+" is not avalaible at our Bakery.");
+// }
+// 12th question
+
+// 13th question
+// var pass = prompt("Enter password");
+// var length = pass.length;
+// if(pass[0]==1 || pass[0]==2 || pass[0]==3 || pass[0]==4 || pass[0]==5 || pass[0]==6 || pass[0]==7 || pass[0]==8 || pass[0]==9){
+//     alert("Password can not start with numbers \n please enter a valid password");
+// }
+// else if (length>6){
+//     alert("Password can not be greater than 6 characters \n please enter a valid password");
+// }
+// else if (length<6){
+//     alert("Password can not be less than 6 characters \n please enter a valid password");
+// }
+// else if (pass[1]!=1 || pass[1]!=2 || pass[1]!=3 || pass[1]!=4 || pass[1]!=5 ||
+//      pass[1]!=6 || pass[1]!=7 || pass[1]!=8 || pass[1]!=9 && pass[2]!=1 ||
+//      pass[2]!=2 || pass[1]!=3 || pass[2]!=4 || pass[2]!=5 || pass[2]!=6 ||
+//      pass[2]!=7 || pass[2]!=8 || pass[2]!=9 && pass[3]!=1 || pass[3]!=2 ||
+//      pass[3]!=3 || pass[3]!=4 || pass[3]!=5 || pass[3]!=6 || pass[3]!=7 ||
+//      pass[3]!=8 || pass[3]!=9 && pass[4]!=1 || pass[4]!=2 || pass[4]!=3 ||
+//      pass[4]!=4 || pass[4]!=5 || pass[4]!=6 || pass[4]!=7 || pass[4]!=8 ||
+//      pass[4]!=9 && pass[5]!=1 || pass[5]!=2 || pass[5]!=3 || pass[5]!=4 ||
+//      pass[5]!=5 || pass[5]!=6 || pass[5]!=7 || pass[5]!=8 || pass[5]!=9 ){
+//     alert("Password should contain atleast one number \n please enter a valid password");
+// }
+// else{
+//     alert("Good Job");
+// }
+// 13th question
+
+// 14th question
+// var university = "University of Karachi";
+// document.write(university[0] + "<br>"+
+//     university[1] + "<br>"+
+//     university[2] + "<br>"+
+//     university[3] + "<br>"+
+//     university[4] + "<br>"+
+//     university[5] + "<br>"+
+//     university[6] + "<br>"+
+//     university[7] + "<br>"+
+//     university[8] + "<br>"+
+//     university[9] + "<br>"+
+//     university[10] + "<br>"+
+//     university[11] + "<br>"+
+//     university[12] + "<br>"+
+//     university[13] + "<br>"+
+//     university[14] + "<br>"+
+//     university[15] + "<br>"+
+//     university[16] + "<br>"+
+//     university[17] + "<br>"+
+//     university[18] + "<br>"+
+//     university[19] + "<br>"+
+//     university[20] + "<br>"
+// ) 
+// 14th question
+
+// 15th question
+// var input = prompt("enter something");
+// var length = (input.length-1);
+// alert("Last character of input : "+ input[length]);
+// 15th question
+
